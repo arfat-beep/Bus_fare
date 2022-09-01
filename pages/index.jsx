7cec9f6368a6aa1d77a8d09cb4709c96c437a7cb
arfat-beep <arfatrahman08@gmail.com>
2022-09-02 01:01:51 +0600

    trying to solve vercel error

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import Fare from "../components/Fare";
 
 export default function Home() {
   return (
-    <div className="w-11/12 mx-auto grid justify-center pt-5">
+    <div className="w-11/12 mx-auto grid justify-center pt-3">
       <Fare />
     </div>
   );
